738ef43285d733bcd90fb305e2261f2cbd2143c8
kashipu <graficowm@gmail.com>
2020-05-23 01:53:00 -0500

    V-0.1 Init repo, Grid Structure

---
+++ js/main.js
@@ -0,0 +1,7 @@
+function toggle() {
+    let navBar = document.querySelector('#navToggle');
+    let contentBox = document.querySelector('#contentBox')
+    let footerBox = document.querySelector('#footerBox')
+    navBar.classList.toggle('show-ico');
+}
+
