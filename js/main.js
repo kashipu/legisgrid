9a4af4a25ab0ce3a192abce9dac7bc1332fd90ec
kashipu <graficowm@gmail.com>
2020-05-24 13:11:30 -0500

    V-0.2 Nav Responsive, ficha y tooltip

--- js/main.js
+++ js/main.js
@@ -2,9 +2,11 @@ function toggle() {
     let navBar = document.querySelector('#navToggle');
     let contentBox = document.querySelector('#contentBox')
     let footerBox = document.querySelector('#footerBox')
+    let helpTextBox = document.querySelector('#helpText')
     navBar.classList.toggle('show-ico');
     contentBox.classList.toggle('none-ico');
     footerBox.classList.toggle('none-ico');
+    helpTextBox.classList.toggle('none-ico');
 
 }
 
