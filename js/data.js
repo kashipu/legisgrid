892a46f873b4096e6a6d3f216394cb8a28d0ee4d
kashipu <graficowm@gmail.com>
2020-05-31 03:38:25 -0500

    V-0.6 Responsive, javascrip data and colors

---
+++ js/data.js
@@ -0,0 +1,241 @@
+const summarAll = `<li>Vendedor: Persona encargada de la venta o comercialización de un producto.</li>
+<li>Transporte interno: es el desplazamiento que se realiza desde la fábrica hasta un punto
+    acordado.</li>
+<li>Aduana origen: trámites que se deben realizar para la autorización de exportación de la
+    mercancía.</li>
+<li>operación en puerto, aeropuerto o pasos de fronteras según sea el caso: son todos los
+    procedimiento de manipulación de la mercancía para el cargue al transporte internacional.
+</li>
+<li>seguro: contrato que se efectúa con una agencia de seguros con el fin de transferir el
+    riesgo de alguna eventualidad con relación de la mercancía.</li>
+<li>flete: costo que se genera al contratar el transporte (local o internacional) de acuerdo a
+    su negociación.</li>
+<li>tránsito internacional: transporte de la mercancía desde el país de origen a país de
+    destino.</li>
+<li>operación en puerto, aeropuerto o pasos de fronteras según sea el caso: son todos los
+    procedimiento de manipulación de la mercancía para el descargue al transporte internacional.
+</li>
+<li>aduana destino: trámites que se deben realizar para la autorización de la importacion y
+    legalizacion de la entrada de la mercancía en el país de destino.</li>
+<li>transporte interno: es el desplazamiento que se realiza para la entrega de la mercancía al
+    cliente o comprador.</li>
+<li>comprador: la persona encargada de negociar o contratar la compra de un producto en el
+    exterior.</li>
+<li>país de compra: país donde se efectúa la compra de la mercancía.
+<li>País de origen: lugar donde se fabrica la mercancía.
+<li>puerto de embarque: lugar donde se despacha la mercancía.
+<li>país de destino: lugar final donde se recibe la mercancía.</li>`
+
+
+
+const info = [{
+        sigla: "EXW",
+        subtitulo: "Exworks en fábrica",
+        descripcion: "El vendedor únicamente es responsable de entregar la mercancía en el lugar designadas por el mismo bien sea (fábrica, almacén, taller etc.) y  el comprador asume todos los riesgos y los costos desde el momento que recibe la mercancía hasta su destino.",
+        imagen: "http://via.placeholder.com/450X450",
+        checklisk: `<li>El vendedor indica el lugar de entrega de la mercancía.</li>
+            <li>El vendedor no está obligado a realizar el cargue de la mercancía.</li>
+            <li>El vendedor debe entregar el producto listo para despacho, con embalaje y rotulado
+                necesario.</li>
+            <li>El comprador debe asumir los costos al momento de entrega de la mercancía no
+            cargada.</li>
+            <li>Se recomienda que se pacte el cargue de la carga ya que usualmente el vendedor
+                    dispone de los recursos necesarios para la correcta manipulación de la carga.</li>`,
+        glosario: summarAll
+    },
+    {
+        sigla: "FCA",
+        subtitulo: "Free Carrier / Franco Porteador",
+        descripcion: "Se debe acordar o concretar un punto de entrega de la mercancía al transportista (transitario, naviera, aerolínea, etc.)  elegido por el comprador, el vendedor debe hacer la entrega de la mercancía una vez superada la aduana de exportación, asumiendo los gastos generado por manipulación, transporte, descargue y costos aduaneros desde el almacén o fábrica hasta el lugar o punto fijado para ello.",
+        imagen: "http://via.placeholder.com/450X450",
+        checklisk: `<li>El vendedor indica el lugar de entrega de la mercancía.</li>
+            <li>El vendedor no está obligado a realizar el cargue de la mercancía.</li>
+            <li>El vendedor debe entregar el producto listo para despacho, con embalaje y rotulado
+                necesario.</li>
+            <li>El comprador debe asumir los costos al momento de entrega de la mercancía no
+            cargada.</li>
+            <li>Se recomienda que se pacte el cargue de la carga ya que usualmente el vendedor
+                    dispone de los recursos necesarios para la correcta manipulación de la carga.</li>`,
+        glosario: summarAll,
+    },
+    {
+        sigla: "CPT",
+        subtitulo: "Carriage Paid To / Transporte pagado hasta",
+        descripcion: "El vendedor hace la entrega de la mercancía y asume el costo del transporte hasta un puerto o destino acordado, incluyendo todos los gastos, trámites de exportación, despacho aduanero y transporte internacional. No obstante la responsabilidad y el riesgo la asume el comprador desde que inicia el transporte internacional",
+        imagen: "http://via.placeholder.com/450X450",
+        checklisk: `<li>El vendedor indica el lugar de entrega de la mercancía.</li>
+            <li>El vendedor no está obligado a realizar el cargue de la mercancía.</li>
+            <li>El vendedor debe entregar el producto listo para despacho, con embalaje y rotulado
+                necesario.</li>
+            <li>El comprador debe asumir los costos al momento de entrega de la mercancía no
+            cargada.</li>
+            <li>Se recomienda que se pacte el cargue de la carga ya que usualmente el vendedor
+                    dispone de los recursos necesarios para la correcta manipulación de la carga.</li>`,
+        glosario: summarAll
+    },
+    {
+        sigla: "CIP",
+        subtitulo: "Carriage and Insurance Paid to / Transporte y Seguro Pagados hasta",
+        descripcion: "Es el complemento del Incoterm®  CPT, se incluye el valor del seguro que es asumido por el vendedor. El vendedor debe asumir todos los gastos relacionados con manipulación, transporte local, trámites de exportación, despacho aduanero, transporte internacional y seguro. No obstante la responsabilidad del comprador inicia al finalizar el transporte internacional.",
+        imagen: "http://via.placeholder.com/450X450",
+        checklisk: `<li>El vendedor indica el lugar de entrega de la mercancía.</li>
+            <li>El vendedor no está obligado a realizar el cargue de la mercancía.</li>
+            <li>El vendedor debe entregar el producto listo para despacho, con embalaje y rotulado
+                necesario.</li>
+            <li>El comprador debe asumir los costos al momento de entrega de la mercancía no
+            cargada.</li>
+            <li>Se recomienda que se pacte el cargue de la carga ya que usualmente el vendedor
+                    dispone de los recursos necesarios para la correcta manipulación de la carga.</li>`,
+        glosario: summarAll
+    },
+    {
+        sigla: "DAP",
+        subtitulo: "Delivered At Place / Entregada en Lugar Convenido",
+        descripcion: "El vendedor entrega la mercancía y asume los costos generados en origen tales como (transporte, cargue, aduana exportación, transporte internacional, seguro y demás movimientos ocasionados para lograr el despacho de la mercancía) y costos en destino como ( gastos por manipulación y movimiento de la mercancía y transporte en destino hasta entregar la mercancía en un punto acordado).  el comprador debe asumir los costos y gastos en aduana de importación.",
+        imagen: "http://via.placeholder.com/450X450",
+        checklisk: `<li>El vendedor indica el lugar de entrega de la mercancía.</li>
+            <li>El vendedor no está obligado a realizar el cargue de la mercancía.</li>
+            <li>El vendedor debe entregar el producto listo para despacho, con embalaje y rotulado
+                necesario.</li>
+            <li>El comprador debe asumir los costos al momento de entrega de la mercancía no
+            cargada.</li>
+            <li>Se recomienda que se pacte el cargue de la carga ya que usualmente el vendedor
+                    dispone de los recursos necesarios para la correcta manipulación de la carga.</li>`,
+        glosario: summarAll
+    },
+    {
+        sigla: "DPU",
+        subtitulo: "Delivered at place unloaded / Mercancía entregada y descargada",
+        descripcion: "El vendedor entrega la mercancía hasta el sitio en destino, cubriendo todos los gastos generados en transporte local, internacional, seguro, aduana de exportación y manipulación de carga en origen y destino, transporte en destino. el comprador debe cubrir los gastos generados en aduana de importación y pago de impuestos.",
+        imagen: "http://via.placeholder.com/450X450",
+        checklisk: `<li>El vendedor indica el lugar de entrega de la mercancía.</li>
+            <li>El vendedor no está obligado a realizar el cargue de la mercancía.</li>
+            <li>El vendedor debe entregar el producto listo para despacho, con embalaje y rotulado
+                necesario.</li>
+            <li>El comprador debe asumir los costos al momento de entrega de la mercancía no
+            cargada.</li>
+            <li>Se recomienda que se pacte el cargue de la carga ya que usualmente el vendedor
+                    dispone de los recursos necesarios para la correcta manipulación de la carga.</li>`,
+        glosario: summarAll
+    },
+    {
+        sigla: "DDP",
+        subtitulo: "Delivered Duty Paid / Entrega con Derechos Pagados",
+        descripcion: "El DDP establece que el vendedor cubre todos los gastos ocasionados tanto en origen como en destino, excluyendo el descargue en destino, el vendedor adquiere el riesgo una vez la mercancía sea entregada o llegue al sitio acordado en destino.",
+        imagen: "http://via.placeholder.com/450X450",
+        checklisk: `<li>El vendedor indica el lugar de entrega de la mercancía.</li>
+            <li>El vendedor no está obligado a realizar el cargue de la mercancía.</li>
+            <li>El vendedor debe entregar el producto listo para despacho, con embalaje y rotulado
+                necesario.</li>
+            <li>El comprador debe asumir los costos al momento de entrega de la mercancía no
+            cargada.</li>
+            <li>Se recomienda que se pacte el cargue de la carga ya que usualmente el vendedor
+                    dispone de los recursos necesarios para la correcta manipulación de la carga.</li>`,
+        glosario: summarAll
+    },
+    {
+        sigla: "FAS",
+        subtitulo: "Free Alongside Ship / Franco al Costado del Buque",
+        descripcion: "El vendedor se hace cargo de la entrega de la mercancía en el puerto de embarque, es decir el vendedor asume los costos generado hasta la entrega de la mercancía en puerto (transporte en origen, aduana  de exportación y demás gastos que se generen al ingresar y dejar la mercancía a disposición del puerto), el comprador asume el riesgo una vez sea transferida la responsabilidad en el puerto de origen hasta la entrega de la mercancía en destino, debe asumir los gastos generados tales como (manipulación de carga en  puerto origen, flete internacional, seguro, manipulación de carga en destino, aduana y transporte local).",
+        imagen: "http://via.placeholder.com/450X450",
+        checklisk: `<li>El vendedor indica el lugar de entrega de la mercancía.</li>
+            <li>El vendedor no está obligado a realizar el cargue de la mercancía.</li>
+            <li>El vendedor debe entregar el producto listo para despacho, con embalaje y rotulado
+                necesario.</li>
+            <li>El comprador debe asumir los costos al momento de entrega de la mercancía no
+            cargada.</li>
+            <li>Se recomienda que se pacte el cargue de la carga ya que usualmente el vendedor
+                    dispone de los recursos necesarios para la correcta manipulación de la carga.</li>`,
+        glosario: summarAll
+    },
+    {
+        sigla: "FOB",
+        subtitulo: "Free On Board / Franco a Bordo",
+        descripcion: "El vendedor hace la entrega de la mercancía a bordo del buque, cubriendo los gastos generados en origen tales como (transporte local, aduana de exportación y manipulación de mercancía)  Asimismo, el comprador asume  todos los costos y riesgos de daño o pérdida desde ese lugar. (flete internacional, seguro, manipulacion de mercancia, aduana de importación y transporte local en destino).",
+        imagen: "http://via.placeholder.com/450X450",
+        checklisk: `<li>El vendedor indica el lugar de entrega de la mercancía.</li>
+            <li>El vendedor no está obligado a realizar el cargue de la mercancía.</li>
+            <li>El vendedor debe entregar el producto listo para despacho, con embalaje y rotulado
+                necesario.</li>
+            <li>El comprador debe asumir los costos al momento de entrega de la mercancía no
+            cargada.</li>
+            <li>Se recomienda que se pacte el cargue de la carga ya que usualmente el vendedor
+                    dispone de los recursos necesarios para la correcta manipulación de la carga.</li>`,
+        glosario: summarAll
+    },
+    {
+        sigla: "CFR",
+        subtitulo: "Cost and Freight / Coste y Flete",
+        descripcion: "Incoterms utilizado únicamente para transporte por marítimo o fluvial, en este caso el vendedor asume los costos generados  hasta entregar la mercancía en puerto de destino tales como (transporte local en origen, aduana de exportación, manipulación de carga en puerto, y flete internacional), de este modo transfiere el riesgo al comprador una vez esté la mercancía en puerto de destino, de este modo el comprador asume los gastos ocasionados como (seguro, manipulación de carga en puerto de destino, aduana de importación y transporte local en destino).",
+        imagen: "http://via.placeholder.com/450X450",
+        checklisk: `<li>El vendedor indica el lugar de entrega de la mercancía.</li>
+            <li>El vendedor no está obligado a realizar el cargue de la mercancía.</li>
+            <li>El vendedor debe entregar el producto listo para despacho, con embalaje y rotulado
+                necesario.</li>
+            <li>El comprador debe asumir los costos al momento de entrega de la mercancía no
+            cargada.</li>
+            <li>Se recomienda que se pacte el cargue de la carga ya que usualmente el vendedor
+                    dispone de los recursos necesarios para la correcta manipulación de la carga.</li>`,
+        glosario: summarAll
+    },
+    {
+        sigla: "CIF",
+        subtitulo: "Cost, Insurance and Freight/Coste, Seguro y Flete",
+        descripcion: "Es el complemento del CFR, donde se adiciona el seguro marítimo a cargo del vendedor, en este caso el vendedor debe asumir los costos generado en origen sumando el flete internacional y el seguro, (transporte local en origen, aduana de exportación, manipulación de carga en puerto, flete internacional y seguro), el comprador asume los gastos ocasionados como (manipulación de carga en puerto de destino, aduana de importación y transporte local en destino).",
+        imagen: "http://via.placeholder.com/450X450",
+        checklisk: `<li>El vendedor indica el lugar de entrega de la mercancía.</li>
+            <li>El vendedor no está obligado a realizar el cargue de la mercancía.</li>
+            <li>El vendedor debe entregar el producto listo para despacho, con embalaje y rotulado
+                necesario.</li>
+            <li>El comprador debe asumir los costos al momento de entrega de la mercancía no
+            cargada.</li>
+            <li>Se recomienda que se pacte el cargue de la carga ya que usualmente el vendedor
+                    dispone de los recursos necesarios para la correcta manipulación de la carga.</li>`,
+        glosario: summarAll
+    }
+]
+
+function exw(){
+    colores(1, conven1, conven2);
+    colores(2, conven3, conven4, ribbonNone);
+    colores(3, conven3, conven4, ribbonNone);
+    colores(4, conven3, conven4, ribbonNone);
+    colores(5, conven3, conven4, ribbonNone);
+    colores(6, conven3, conven4, ribbonNone);
+    colores(7, conven3, conven4, ribbonNone);
+    colores(8, conven3, conven4, ribbonNone);
+    colores(9, conven3, conven4, ribbonNone);
+    colores(10, conven3, conven4, ribbonNone);
+    colores(11, conven3, conven4, ribbonNone);
+    colores(12, conven3, conven4, ribbonNone);
+}
+
+function fca(){
+    colores(1, conven1, conven2);
+    colores(2, conven1, conven2);
+    colores(3, conven5, conven5, ribbonNone);
+    colores(4, conven1, conven2, ribbonNone);
+    colores(5, conven3, conven4, ribbonNone);
+    colores(6, conven3, conven4, ribbonNone);
+    colores(7, conven3, conven4, ribbonNone);
+    colores(8, conven3, conven4, ribbonNone);
+    colores(9, conven3, conven4, ribbonNone);
+    colores(10, conven3, conven4, ribbonNone);
+    colores(11, conven3, conven4, ribbonNone);
+    colores(12, conven3, conven4, ribbonNone);
+}
+
+function cpt(){
+    colores(1, conven1, conven2);
+    colores(2, conven1, conven2);
+    colores(3, conven1, conven2, ribbonNone);
+    colores(4, conven1, conven2, ribbonNone);
+    colores(5, conven1, conven2, ribbonNone);
+    colores(6, conven1, conven4, ribbonNone);
+    colores(7, conven3, conven4, ribbonNone);
+    colores(8, conven3, conven4, ribbonNone);
+    colores(9, conven3, conven4, ribbonNone);
+    colores(10, conven3, conven4, ribbonNone);
+    colores(11, conven3, conven4, ribbonNone);
+    colores(12, conven3, conven4, ribbonNone);
+}
--- js/main.js
+++ js/main.js
@@ -1,13 +1,107 @@
-function toggle() {
-    let navBar = document.querySelector('#navToggle');
-    let contentBox = document.querySelector('#contentBox')
-    let footerBox = document.querySelector('#footerBox')
-    let helpTextBox = document.querySelector('#helpText')
-    navBar.classList.toggle('show-ico');
-    contentBox.classList.toggle('none-ico');
-    footerBox.classList.toggle('none-ico');
-    helpTextBox.classList.toggle('none-ico');
+let conven1 = 'colorConven1'
+let conven2 = 'colorConven2'
+let conven3 = 'colorConven3'
+let conven4 = 'colorConven4'
+let conven5 = 'colorConven5'
+let ribbonNone = 'none-ico'
 
+function colores(i, colorBox1, colorBox2, ribbon) {
+    let color1 = document.querySelector(`.scroll-card-inc:nth-of-type(${i}) .scroll-color-box-1`);
+    let color2 = document.querySelector(`.scroll-card-inc:nth-of-type(${i}) .scroll-color-box-2`);
+    let ribbonShow = document.querySelector(`.scroll-card-inc:nth-of-type(${i}) .scroll-ribbon`);
+    color1.classList.remove("colorConven1", "colorConven2", "colorConven3", "colorConven4", "colorConven5")
+    color2.classList.remove("colorConven1", "colorConven2", "colorConven3", "colorConven4", "colorConven5")
+    ribbonShow.classList.remove("none-ico")
+    color1.classList.add(colorBox1)
+    color2.classList.add(colorBox2)
+    ribbonShow.classList.add(ribbon)
 }
 
+function audioPlay() {
+    let player = document.querySelector('#audioTag')
+    player.play()
+}
 
+function toggle() {
+    let queryX = window.matchMedia("(max-width:1110px)")
+    let navBar = document.querySelector('#navToggle');
+    navBar.classList.toggle('show-ico');
+    if (queryX.matches) {
+        let contentBox = document.querySelector('#contentBox')
+        let footerBox = document.querySelector('#footerBox')
+        let helpTextBox = document.querySelector('#helpText')
+        contentBox.classList.toggle('none-ico');
+        footerBox.classList.toggle('none-ico');
+        helpTextBox.classList.toggle('none-ico');
+    }
+}
+function insertInfo(i) {
+    console.log(i)
+    //titulo
+    let incTitle = document.querySelector('#inc-title')
+    incTitle.innerText = info[i].sigla
+    //Subtitutulo
+    let incSubTitle = document.querySelector('#inc-subtitle')
+    incSubTitle.innerHTML = info[i].subtitulo
+    //Descripción
+    let incDescrip = document.querySelector('#inc-descrip')
+    incDescrip.innerHTML = info[i].descripcion
+    // imagen
+    let incImg = document.querySelector('#inc-img')
+    incImg.setAttribute("src", info[i].imagen)
+    //Checklist
+    let incChecklist = document.querySelector('#inc-checklist')
+    incChecklist.innerHTML = info[i].checklisk;
+    //Glosario
+    let incSumm = document.querySelector('#inc-summ')
+    incSumm.innerHTML = info[i].glosario
+    //Guia de la página
+    let textGuide = document.querySelector('#incTextGuide')
+    textGuide.innerHTML = `Página <strong>${i+1} / 11</strong>`
+    // Botones
+    let buttonsGuide = document.querySelector('#buttonsGuide')
+    buttonsGuide.innerHTML = `
+        <button onclick="insertInfo(${i - 1})" class="btn-inc-blue">Anterior</button>
+        <button onclick="insertInfo(${i + 1})" class="btn-inc-blue2">Siguiente</button>
+        `
+    // color help text
+    if ((i == 7) || (i == 8) || (i == 9) || (i == 10)) {
+        let helpTextBox = document.querySelector('#helpText')
+        let fichaBox = document.querySelector('#ficha')
+        let iconBox = document.querySelector('#iconBox')
+        iconBox.style.setProperty('color', '#FFB71B')
+        fichaBox.classList.add('ficha-yellow')
+        helpTextBox.innerHTML = `<h2>TRANSPORTE MARITIMO Y VÍAS NAVEGABLES</h2>`
+        helpTextBox.classList.add('yellow')
+    } else {
+        let helpTextBox = document.querySelector('#helpText')
+        let fichaBox = document.querySelector('#ficha')
+        let iconBox = document.querySelector('#iconBox')
+        iconBox.style.setProperty('color', '#0066FF')
+        helpTextBox.innerHTML = `<h2>OTROS MODOS DE TRANSPORTE</h2>`
+        helpTextBox.classList.remove('yellow')
+        fichaBox.classList.remove('ficha-yellow')
+    }
+
+    if(i == 0) {
+        exw()
+    }else if (i == 1) {
+        fca()
+    }else if (i == 2) {
+        cpt()
+    }
+
+    //Mostrar eventos frente al usar los botones del nav
+    let queryX = window.matchMedia("(max-width:1110px)");
+    if (queryX.matches) {
+        let navBar = document.querySelector('#navToggle');
+        navBar.classList.remove('show-ico');
+        let contentBox = document.querySelector('#contentBox')
+        let footerBox = document.querySelector('#footerBox')
+        let helpTextBox = document.querySelector('#helpText')
+        contentBox.classList.remove('none-ico');
+        footerBox.classList.remove('none-ico');
+        helpTextBox.classList.remove('none-ico');
+    }
+}
+insertInfo(0)
