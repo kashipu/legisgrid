5c985c8f39324b58e7824e7094e0d15c6dc2fcc8
kashipu <graficowm@gmail.com>
2020-07-29 13:10:25 -0500

    V-0.11.3 Correccion Line Height

--- js/main.js
+++ js/main.js
@@ -5,24 +5,6 @@ let conven4 = 'colorConven4'
 let conven5 = 'colorConven5'
 let ribbonNone = 'none-ico'
 
-engine = null;
-if (window.navigator.appName == "Microsoft Internet Explorer")
-{
-   // This is an IE browser. What mode is the engine in?
-   if (document.documentMode) // IE8
-      engine = document.documentMode;
-   else // IE 5-7
-   {
-      engine = 5; // Assume quirks mode unless proven otherwise
-      if (document.compatMode)
-      {
-         if (document.compatMode == "CSS1Compat")
-            engine = 7; // standards mode
-      }
-   }
-   // the engine variable now contains the document compatibility mode.
-}
-
 function colores(i, colorBox1, colorBox2, ribbon) {
     let color1 = document.querySelector(`.scroll-card-inc:nth-of-type(${i}) .scroll-color-box-1`);
     let color2 = document.querySelector(`.scroll-card-inc:nth-of-type(${i}) .scroll-color-box-2`);
